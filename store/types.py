0ff090e3614d54cec87732800fafa9d04b19852a
Adel Mohamadzade <adel.mohamadzadeph@gmail.com>
2022-09-05 08:37:38 +0430

    Implement collection type.

---
+++ store/types.py
@@ -0,0 +1,19 @@
+from graphene import relay
+from graphene_django import DjangoObjectType
+from .models import Product, Collection, Review, Cart, Promotion
+from django.contrib.auth import get_user_model
+
+class CollectionType(DjangoObjectType):
+    class Meta:
+        fields = ('id', 'title', 'products', 'featured_product')
+        filter_fields = {
+            'title': ['exact', 'icontains', 'istartswith'],
+            'id': ['exact']
+        }
+        interfaces = (relay.Node, )
+        model = Collection
+
+    @classmethod
+    def get_queryset(cls, queryset, info):
+        return queryset.prefetch_related('products')
+
