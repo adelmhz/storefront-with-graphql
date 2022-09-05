a9dcc8c28d89cd23568a5420c4b8c777c0d863eb
Adel Mohamadzade <adel.mohamadzadeph@gmail.com>
2022-09-05 08:44:41 +0430

    Implement jwt authentication.

--- core/schema.py
+++ core/schema.py
@@ -9,7 +9,16 @@ class Query(CollectionQuery):
 
 
 class Mutation(CollectionMutation):
-    pass
+    token_auth = graphql_jwt.relay.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.relay.Verify.Field()
+    refresh_token = graphql_jwt.relay.Refresh.Field()
+    delete_token_cookie = graphql_jwt.relay.DeleteJSONWebTokenCookie.Field()
+
+    # Long running refresh tokens
+    revoke_token = graphql_jwt.relay.Revoke.Field()
+
+    delete_refresh_token_cookie = \
+        graphql_jwt.relay.DeleteRefreshTokenCookie.Field()
 
 
 
